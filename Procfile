79644ea0dfa5d306d18a2f45fa408af438db85a4
Fairus In'am Pratama <fairusinam08@gmail.com>
2025-06-05 17:31:18 +0700

    Fix server configuration and restore proper Laravel bootstrap

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: php -d display_errors=1 -d error_reporting=E_ALL -S 0.0.0.0:$PORT -t public
+web: php -d display_errors=1 -d error_reporting=E_ALL -S 0.0.0.0:$PORT public/index.php
 worker: php artisan queue:work --tries=3 --timeout=90
--- public/index.php
+++ public/index.php
@@ -14,4 +14,21 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
